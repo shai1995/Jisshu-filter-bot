749567815b676ae3af68bf38445b00928e50f723
shai1995 <76399884+shai1995@users.noreply.github.com>
2024-10-15 13:17:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/MRADBOT_OFFICIALS">MRAdbot_Officials</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
